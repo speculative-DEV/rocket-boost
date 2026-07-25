809a8fccb7a87fa34f3a4cb8642e2c64f972f1fa
speculative-DEV <wilhelmid77@gmail.com>
2026-07-25 17:02:10 +0100

    Feat: applying the restart and quit after winning

--- player.gd
+++ player.gd
@@ -1,12 +1,33 @@
 extends RigidBody3D
 
+## How much vertical force to apply when moving.
+@export_range(750.0, 3000.0) var thrust: float = 1000.0
+
+## How much angular force to apply when rotated.
+@export var torque_thrust: float = 100.0
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	if Input.is_action_pressed("boost"):
-		apply_central_force(basis.y * delta * 1000.0)
+		apply_central_force(basis.y * delta * thrust)
 	
 	if Input.is_action_pressed("rotate_left"):
-		apply_torque(Vector3(0.0, 0.0, 100.0 * delta))
+		apply_torque(Vector3(0.0, 0.0, torque_thrust * delta))
 	
 	if Input.is_action_pressed("rotate_right"):
-		apply_torque(Vector3(0.0, 0.0, -100.0 * delta))
+		apply_torque(Vector3(0.0, 0.0, -torque_thrust * delta))
+
+
+func _on_body_entered(body: Node) -> void:
+	if "Goal" in body.get_groups():
+		complete_level() 
+	if "Hazard" in body.get_groups():
+		crash_sequence() 	
+
+func crash_sequence() -> void:
+	print("Kaboom!")
+	get_tree().reload_current_scene()
+
+func complete_level() -> void:
+	print("Level Complete!")
+	get_tree().quit()
