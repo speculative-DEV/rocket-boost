0edc7441660daff3f91e2c748f7cd1ac976c4322
speculative-DEV <wilhelmid77@gmail.com>
2026-07-28 22:10:07 +0100

    Feat: animating the transition between scenes and restarting the scene

--- player.gd
+++ player.gd
@@ -6,6 +6,8 @@ extends RigidBody3D
 ## How much angular force to apply when rotated.
 @export var torque_thrust: float = 100.0
 
+var is_transitioning: bool = false
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	if Input.is_action_pressed("boost"):
@@ -19,15 +21,24 @@ func _process(delta: float) -> void:
 
 
 func _on_body_entered(body: Node) -> void:
-	if "Goal" in body.get_groups():
-		complete_level(body.file_path) 
-	if "Hazard" in body.get_groups():
-		crash_sequence() 	
+	if is_transitioning == false:
+		if "Goal" in body.get_groups():
+			complete_level(body.file_path) 
+		if "Hazard" in body.get_groups():
+			crash_sequence() 	
 
 func crash_sequence() -> void:
 	print("Kaboom!")
-	get_tree().reload_current_scene()
+	set_process(false)
+	is_transitioning = true 
+	var tween = create_tween()
+	tween.tween_interval(1.0)
+	tween.tween_callback(get_tree().reload_current_scene)
 
 func complete_level(next_level_file: String) -> void:
 	print("Level Complete!")
-	get_tree().change_scene_to_file(next_level_file)
+	set_process(false)
+	is_transitioning = true
+	var tween = create_tween()
+	tween.tween_interval(1.0)
+	tween.tween_callback(get_tree().change_scene_to_file.bind(next_level_file))
