40099094e71a03f1654f9aa638c64cfe62fbe496
speculative-DEV <wilhelmsid77@gmail.com>
2026-07-31 19:04:14 +0100

    Feat: attaching escape key event to close the game

--- player.gd
+++ player.gd
@@ -39,6 +39,9 @@ func _process(delta: float) -> void:
 		left_booster_particles.emitting = true
 	else:
 		left_booster_particles.emitting = false	
+	
+	if Input.is_action_pressed("ui_cancel"):
+		get_tree().quit()
 
 func _on_body_entered(body: Node) -> void:
 	if is_transitioning == false:
